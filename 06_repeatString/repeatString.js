845a3e5760dd8fb0cf82c64046e50cbfbfca848f
Ved Pawar <vedpawar1410@gmail.com>
2025-09-26 22:17:34 +0530

    06-repeat-string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let tempStr='';
+    for (let i = 0; i < num; i++) {
+        tempStr+=str;
+    }
+    return tempStr;
 };
 
 // Do not edit below this line
