09b939d9f0afb8dc93fc85813b725e05bc4bedaf
Ved Pawar <vedpawar1410@gmail.com>
2025-09-26 22:22:04 +0530

    06-repeatString : All test cases passed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,9 +1,15 @@
 const repeatString = function(str,num) {
     let tempStr='';
-    for (let i = 0; i < num; i++) {
-        tempStr+=str;
+    if(num==-1){
+        return 'ERROR';
     }
-    return tempStr;
+    else{
+        for (let i = 0; i < num; i++) {
+            tempStr+=str;
+        }
+        return tempStr;
+    }
+    
 };
 
 // Do not edit below this line
