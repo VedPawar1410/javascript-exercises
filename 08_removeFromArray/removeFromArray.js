76f99448b503a02c1b346156a5a6d5b0a466d512
Ved Pawar <vedpawar1410@gmail.com>
2025-09-27 09:34:51 +0530

    08-removeFromArray- all test cases passed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function(arr, num) {
-    let splicedArray = arr.splice(arr.indexOf(num),1);
-    return arr;
+const removeFromArray = function(arr, ...num) {
+    //let splicedArray = arr.splice(arr.indexOf(num),1);
+    return arr.filter((item)=>(!num.includes(item)));
 };
 
 // Do not edit below this line
