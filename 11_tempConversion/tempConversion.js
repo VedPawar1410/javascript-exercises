00317922b0befcd93ee1ed764b7979f246440473
Ved Pawar <vedpawar1410@gmail.com>
2025-09-27 11:11:56 +0530

    11-tempConversion - all test cases passed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFaran) {
+  return Math.round(((tempFaran-32) * (5/9))*10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  return Math.round(((tempCelsius*(9/5) + 32))*10) / 10;
 };
 
 // Do not edit below this line
