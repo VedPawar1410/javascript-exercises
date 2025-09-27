642030e8de28a2053816507f0cb75079650de295
Ved Pawar <vedpawar1410@gmail.com>
2025-09-27 10:02:34 +0530

    09-sumAll- all test cases passed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(a<0 || b<0 || (a % 1 !== 0) || (b % 1 !== 0) || !(Number.isFinite(a)) || !(Number.isFinite(b)) || Array.isArray(a) ||  Array.isArray(b)){
+        return "ERROR";
+    }
+    /* if(a<b){
+        let start = a;
+        let end = b;
+    }
+    else{
+        let start=b;
+        let end = a;
+    } */
+    let sum=0;
+    for (let i = (a<b ? a:b); i <= (a<b?b:a); i++) {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
